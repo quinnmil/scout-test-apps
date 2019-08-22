5d33bbf25456ac1b61fba7ff140b4273047511a9
Adam Johnson <me@adamj.eu>
2019-08-22 09:11:46 +0100

    Add nameko rpc example

--- nameko/app.py
+++ nameko/app.py
@@ -1,8 +1,19 @@
 # coding=utf-8
+"""
+To start:
+1. docker run -d --name rabbitmq -p 5672:5672 rabbitmq:3.7.17
+2. nameko run app
+
+Then can invoke the service with either:
+1. http://localhost:8000/
+2. nameko shell
+   >>> n.rpc.myservice.hello("me")
+"""
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import os
 
+from nameko.rpc import rpc
 from nameko.web.handlers import http
 
 from scout_apm.api import Config
@@ -28,3 +39,7 @@ class Service(object):
     @http("GET", "/crash/")
     def crash(self, request):
         raise ValueError("BØØM!")  # non-ASCII
+
+    @rpc
+    def hello(self, name):
+        return "Hello, {}!".format(name)
